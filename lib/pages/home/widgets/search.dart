f4c7899d4d39c943becc7e00dbf5dfd9c4d43a45
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-07-11 17:47:55 +0300

    Personnalize search border

--- lib/pages/home/widgets/search.dart
+++ lib/pages/home/widgets/search.dart
@@ -10,7 +10,28 @@ class SearchSection extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25 ),
       child: Stack(
         children: [
-          TextField(),
+          TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none
+                ),
+              ),
+              prefixIcon: const Icon(
+                  Icons.search_outlined,
+                  size: 30,
+              ),
+              hintText: "Search Game",
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey.withOpacity(0.7)
+              )
+            ),
+          ),
           Positioned(
               right: 12,
               bottom: 10,
