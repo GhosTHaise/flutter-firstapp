960a199159321aef9f3d146e514838ddedc9a2c9
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-07-13 14:40:15 +0300

    Place image

--- lib/pages/home/widgets/popular.dart
+++ lib/pages/home/widgets/popular.dart
@@ -1,13 +1,35 @@
 import "package:flutter/material.dart";
+import "package:initialiasation_1/models/game.dart";
 
 class PopularGame extends StatelessWidget {
-  const PopularGame({Key? key}) : super(key: key);
+  PopularGame({Key? key}) : super(key: key);
+  final List<Game> games = Game.games();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 200,
-      color: Colors.red,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 20),
+        scrollDirection: Axis.horizontal,
+          itemBuilder:(context,index) => GestureDetector(
+            onTap: () => print("On tap !"),
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: ClipRect(
+                    borderRadius : ,
+                    child: Image.asset(games[index].bgImage),
+                ),
+              )),
+          ),
+          separatorBuilder: (context,index) => const SizedBox(width: 10),
+          itemCount: games.length
+      ),
     );
   }
 }
